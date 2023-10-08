b87cf6543fdc5b7efeb721a13a5c81cedf90b1ec
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-10-07 23:09:02 -0300

    Agregar + Edad
    Agregado de funcionalidad

--- gran-inversion/src/Componentes/GranInversion.jsx
+++ gran-inversion/src/Componentes/GranInversion.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./GranInversion.css"
 
 const PersonCard = (props) => {
+
+    let [edadState, setEdadState] = useState (props.edad);
+
+    const clickEdad =() => {
+        setEdadState (edadState += 1);
+    }
+
     return (
         <div className="personCard">
             <h1>{props.apellido}, {props.nombre}</h1>
-            <p>Age: {props.edad}</p>
+            <p>Age: {edadState}</p>
+            <button onClick={clickEdad}>+ Edad</button>
             <p>Hair Color: {props.colorCabello}</p>
         </div>
     );
